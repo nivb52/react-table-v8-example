7cdf1b948514a050336a2e343062b42b1d276b38
nivb52 <nivb52@gmail.com>
2024-08-05 16:12:15 +0300

    module/csoc-list/fix: remove key prop

--- src/modules/CSOCList/TableCell.tsx
+++ src/modules/CSOCList/TableCell.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 interface Props {
     children?: React.ReactNode;
-    key: string;
     style?: React.CSSProperties;
 }
 /**
  * @name TableCell
  * @description 
  */
-export default ({ children, key, style }: Props) => {
+export default ({ children, style }: Props) => {
     return (
-        <td className='table-cell' key={key} style={style}>
+        <td className='table-cell' style={style}>
             {children}
         </td>
     );
